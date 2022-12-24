d8354479ec035d6f67244ff7440a6897324ff930
dendenxu <zhenx@zju.edu.cn>
2022-12-24 20:51:46 +0800

    add a debugging interface

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ from torch import nn
 from glob import glob
 from tqdm import tqdm
 from os.path import join
-from typing import Tuple
 from rich import traceback
+from termcolor import colored
+from typing import Tuple, List
 import matplotlib.pyplot as plt
 
 from kornia.feature.laf import get_laf_pts_to_draw, get_laf_center, get_laf_orientation, get_laf_scale
@@ -21,7 +22,7 @@ from kornia.color import rgb_to_grayscale
 
 import torch.nn.functional as F
 
-from utils import parallel_execution, load_image, load_unchanged, save_image, save_unchanged, list_to_tensor, tensor_to_list, normalize
+from utils import parallel_execution, load_image, load_unchanged, save_image, save_unchanged, list_to_tensor, tensor_to_list, normalize, log
 traceback.install()
 
 # https://github.com/kornia/kornia-examples/blob/master/image-matching-example.ipynb
@@ -67,15 +68,15 @@ def resize_image_tensor(imgs: torch.Tensor, ratio: float):
     B, C, H, W = imgs.shape
     nH, nW = int(H * ratio), int(W * ratio)
     # assert int(nH * ratio) == H and int(nW * ratio) == W, 'resizing ratio should be exact for easy inversion'
-    imgs = F.interpolate(imgs, (nH, nW), mode='area', align_corners=False)
+    imgs = F.interpolate(imgs, (nH, nW), mode='area')
     return imgs
 
 
-def pack_laf_into_opencv_fmt(laf: torch.Tensor, resp: torch.Tensor):
+def pack_laf_into_opencv_fmt(lafs: torch.Tensor, resp: torch.Tensor):
     # return value's last dimension: center x2, scale x1, orientation x1 (in degree)
-    xy = get_laf_center(laf)  # B, N, 2
-    scale = get_laf_scale(laf)  # B, N, 1, 1
-    ori = get_laf_orientation(laf)  # B, N, 1
+    xy = get_laf_center(lafs)  # B, N, 2
+    scale = get_laf_scale(lafs)  # B, N, 1, 1
+    ori = get_laf_orientation(lafs)  # B, N, 1
     packed = torch.cat([xy, scale[..., 0], ori, resp[..., None]], dim=-1)  # B, N, 4
     return packed
 
@@ -89,18 +90,18 @@ def feature_matching(descs: torch.Tensor, match_ratio: float = .9) -> Tuple[int,
 
     # Find the one with cloest match to other images as the pivot (# ? not scalable?)
     min2, match = ssd.topk(2, dim=-1, largest=False)  # find closest distance
-    match = match[..., 0]  # only the largest value corresponde to dist B, N
+    match = match[..., 0]  # only the largest value corresponde to dist B, N, cloest is on diagonal
     dist = min2[..., 0] / min2[..., 1]  # unambiguous match should have low distance here B, B, N,
     pivot = dist.sum(-1).sum(-1).argmin().item()  # find the pivot image to use (diagonal trivially ignored) # MARK: SYNC
 
     # Rearranage images and downscaled images according to pivot image selection
-    match = torch.cat([match[pivot, :pivot], match[pivot, pivot + 1:]])  # B-1, N
-    dist = torch.cat([dist[pivot, :pivot], dist[pivot, pivot + 1:]])  # B-1, N
+    match = torch.cat([match[pivot, :pivot], match[pivot, pivot + 1:]])  # B-1, N, source feat id
+    dist = torch.cat([dist[pivot, :pivot], dist[pivot, pivot + 1:]])  # B-1, N, source feat id
 
     # Select valid threshold -> might not be wise to batch since every image pair is different...
     threshold = dist.ravel().topk(int(dist.numel() * (1 - match_ratio))).values.min()  # the ratio used to discard unmatched values
     matched = (dist < threshold).nonzero()  # M, 2 # MARK: SYNC
-    match = torch.cat([matched, match[matched.chunk(2, dim=-1)]], dim=-1)  # M, 3 # image id, source feat id, target feat id
+    match = torch.cat([matched, match[matched.chunk(2, dim=-1)]], dim=-1)  # M, 3 # image id, pivot feat id, source feat id
     return pivot, match
 
 
@@ -116,15 +117,17 @@ def discrete_linear_transform(pvt: torch.Tensor, src: torch.Tensor):
     # x, y, 1,  0,  0,  0, -x'x, -x'y, -x'
     x, y = src.chunk(2, dim=-1)
     xp, yp = pvt.chunk(2, dim=-1)
-    r0 = torch.cat([torch.zeros_like(x), -x, -y, -torch.ones_like(x), y * xp, y * yp, yp], dim=-1)  # N, 9
-    r1 = torch.cat([x, y, torch.ones_like(x), torch.zeros_like(x), -x * xp, -x * yp, -xp], dim=-1)  # N, 9
+    z = torch.zeros_like(x)
+    o = torch.ones_like(x)
+    r0 = torch.cat([z, z, z, -x, -y, -o, yp * x, yp * y, yp], dim=-1)  # N, 9
+    r1 = torch.cat([x, y, o, z, z, z, -xp * x, -xp * y, -xp], dim=-1)  # N, 9
     A = torch.stack([r0, r1], dim=1).view(-1, 9)  # interlased
 
     U, S, Vh = torch.linalg.svd(A, full_matrices=True)
     V = Vh.mH  # Vh is the conjugate transpose of V
 
     h = V[:, -1]  # 9, the homography
-    H = h.view(3, 3)
+    H = h.view(3, 3) / h[-1]  # normalize homography to 1
     return H  # xp = H x
 
 
@@ -140,83 +143,159 @@ def unique_with_indices(x: torch.Tensor, sorted=False, dim=-1):
     return unique, inverse, counts, indices
 
 
-def transform_and_blending(pvt: torch.Tensor, src: torch.Tensor, homography: torch.Tensor):
+def homography_transform(src: torch.Tensor, homography: torch.Tensor):
     # blends two images with homography transform
     # determine the final size of the blended image to construct a canvas
     # pvt: 3, H, W
     # src: 3, H, W
     # homography: 3, 3
-    C, H, W = pvt.shape
+    C, H, W = src.shape
 
     # straight lines will always map to straight lines
-    corners = pvt.new_tensor([
+    corners = src.new_tensor([
         [0, 0, 1],
         [0, H, 1],
         [W, 0, 1],
         [W, H, 1],
     ])  # 4, 3
     corners = corners @ homography.mT  # 4, 3
-    corners = corners / corners[..., -1:]  # 4, 3 / 4, 1 -> x, y pixels
+    corners = corners[..., :-1] / corners[..., -1:]  # 4, 3 / 4, 1 -> x, y pixels
     min_corner = corners.min(dim=0)[0]  # x, y for min value
     max_corner = corners.max(dim=0)[0]  # x, y for max value
-    src_min_x, src_min_y = int(min_corner[0].floor().item()), int(min_corner[1].floor().item())  # MARK: SYNC
-    src_max_x, src_max_y = int(max_corner[0].ceil().item()), int(max_corner[1].ceil().item())  # MARK: SYNC
-    src_W, src_H = src_max_x - src_min_x, src_max_y - src_min_y
+    min_x, min_y = int(min_corner[0].floor().item()), int(min_corner[1].floor().item())  # MARK: SYNC
+    max_x, max_y = int(max_corner[0].ceil().item()), int(max_corner[1].ceil().item())  # MARK: SYNC
+    src_W, src_H = max_x - min_x, max_y - min_y
 
     # Calculate the pixel coodinates
-    i, j = torch.meshgrid(torch.arange(src_H, dtype=pvt.dtype, device=pvt.device),
-                          torch.arange(src_W, dtype=pvt.dtype, device=pvt.device),
+    i, j = torch.meshgrid(torch.arange(src_H, dtype=src.dtype, device=src.device),
+                          torch.arange(src_W, dtype=src.dtype, device=src.device),
                           indexing='ij')  # H, W
     # 0->H, 0->W
     xy1 = torch.stack([j, i, torch.ones_like(i)], dim=-1)  # mH, mW, 3
     xy1[..., :2] = xy1[..., :2] + min_corner  # shift to origin
-    xy1 = xy1 @ homography.mT  # mH, mW, 3
-    xy1 = xy1 / xy1[..., -1:]  # mH, mW, 3 / mH, mW, 1 -> x, y pixels
-    xy = xy1[..., :2]  # for xy coordinates, mH, mW (x, y inside)
+    xy1 = xy1 @ torch.inverse(homography).mT  # mH, mW, 3
+    xy = xy1[..., :2] / xy1[..., -1:]  # mH, mW, 3 / mH, mW, 1 -> x, y pixels
+    xy = xy / xy.new_tensor([W, H]) * 2 - 1  # normalized to -1, 1
 
     src = F.grid_sample(src[None], xy[None], align_corners=False, padding_mode='zeros', mode='bilinear')[0]  # 3, mH, mW
 
-    # Determine canvas size:
-    pvt_min_x, pvt_min_y = min(0, src_min_x), min(0, src_min_y)
-    pvt_max_x, pvt_max_y = max(W, src_max_x), max(H, src_max_y)
-    pvt_W, pvt_H = pvt_max_x - pvt_min_x, pvt_max_y - pvt_min_y
+    return min_x, min_y, max_x, max_y, src
 
-    # Linear blending for now (excess memory usage?)
-    pvt_canvas = pvt.new_zeros((C, int(pvt_H), int(pvt_W)))  # 3, pH, pW
-    src_canvas = pvt.new_zeros((C, int(pvt_H), int(pvt_W)))  # 3, pH, pW
-    pvt_canvas[-pvt_min_y:H, -pvt_min_x:W] = pvt
-    src_canvas[-pvt_min_y - src_min_y:src_H, -pvt_min_x - src_min_x:src_W] = src
-    blended = (pvt_canvas + src_canvas) / 2
-    return blended
+
+def visualize_detection(imgs: torch.Tensor, lafs: torch.Tensor, resp: torch.Tensor, paths: List[str], ratio=1.0):
+    pack = pack_laf_into_opencv_fmt(lafs, resp)  # B, N, 4
+    pack[..., :2] = pack[..., :2] / ratio
+    imgs = imgs.permute(0, 2, 3, 1)  # B, H, W, C
+    imgs = imgs.detach().cpu().numpy()
+    pack = pack.detach().cpu().numpy()
+    for path, img, kps in zip(paths, imgs, pack):
+        img = (img.clip(0, 1) * 255).astype(np.uint8)[..., ::-1].copy()  # bgr to rgb -> contiguous
+        kps = [cv2.KeyPoint(*d) for d in kps]
+        img = cv2.drawKeypoints(img, kps, img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        cv2.imwrite(path, img)
+
+
+def visualize_matches(imgs_pivot: torch.Tensor,
+                      lafs_pivot: torch.Tensor,
+                      resp_pivot: torch.Tensor,
+                      imgs: torch.Tensor,
+                      lafs: torch.Tensor,
+                      resp: torch.Tensor,
+                      match: torch.Tensor,  # M, 3
+                      paths: List[str],
+                      ratio=1.0):
+    pack_pivot = pack_laf_into_opencv_fmt(lafs_pivot, resp_pivot)  # B, N, 4
+    pack_pivot[..., :2] = pack_pivot[..., :2] / ratio
+    imgs_pivot = imgs_pivot.permute(0, 2, 3, 1)  # B, H, W, C
+    imgs_pivot = imgs_pivot.detach().cpu().numpy()
+    pack_pivot = pack_pivot.detach().cpu().numpy()
+
+    pack = pack_laf_into_opencv_fmt(lafs, resp)  # B, N, 4
+    pack[..., :2] = pack[..., :2] / ratio
+    imgs = imgs.permute(0, 2, 3, 1)  # B, H, W, C
+    imgs = imgs.detach().cpu().numpy()
+    pack = pack.detach().cpu().numpy()
+    _, H, W, C = imgs.shape
+
+    img_pivot = imgs_pivot[0]  # H, W, 3
+    img_pivot = (img_pivot.clip(0, 1) * 255).astype(np.uint8)[..., ::-1].copy()  # bgr to rgb -> contiguous
+    kps_pivot = pack_pivot[0]  # N, 4
+    kps_pivot = [cv2.KeyPoint(*d) for d in kps_pivot]
+
+    match = match[match[..., 0].argsort()]  # sort by image id
+    uni, _, _, ind = unique_with_indices(match[..., 0], sorted=True)
+
+    # From now on, use numpy instead of tensors since we're writing output
+    uni = uni.detach().cpu().numpy()
+    ind = ind.detach().cpu().numpy()
+    match = match.detach().cpu().numpy()
+
+    # For now, only consider matched points of the first image?
+    for idx, curr, next in zip(uni, ind, np.concatenate([ind[1:], np.array([len(match)])])):
+        idx = int(idx)
+        curr = int(curr)
+        next = int(next)
+
+        # Prepare source images
+        path = paths[idx]
+        img = imgs[idx]
+        kps = pack[idx]
+        img = (img.clip(0, 1) * 255).astype(np.uint8)[..., ::-1].copy()  # bgr to rgb -> contiguous
+        kps = [cv2.KeyPoint(*d) for d in kps]
+
+        # Prepare matching parameters
+        match_mask = np.zeros(len(kps_pivot), dtype=np.uint8)
+        match_mask[match[curr:next, 1]] = 1  # pivot selection
+        match_1to2 = np.zeros((len(kps_pivot), 3), dtype=np.uint8)
+        match_1to2[match[curr:next, 1]] = match[curr:next]
+        matches1to2 = [cv2.DMatch(m[1], m[2], m[0], 0) for m in match_1to2]
+
+        canvas = np.zeros([H, 2 * W, 3])
+        canvas = cv2.drawMatches(img_pivot, kps_pivot, img, kps, matches1to2, canvas, matchesMask=match_mask, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)  # weird...
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        cv2.imwrite(path, canvas)
 
 
 def main():
     # Commandline Arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_root', default='data/data1')
+    parser.add_argument('--output_dir', default='output')
+    parser.add_argument('--ext', default='.JPG')
     parser.add_argument('--device', default='cuda')
     parser.add_argument('--ratio', default=0.5, type=float)  # otherwise, typicall out of memory
-    parser.add_argument('--match_ratio', default=0.9, type=float)  # otherwise, typicall out of memory
+    parser.add_argument('--match_ratio', default=0.5, type=float)  # otherwise, typicall out of memory
     args = parser.parse_args()
 
     # Loading images from disk and downscale them
-    imgs = glob(join(args.data_root, '*'))
+    log(f'Loading images from: {args.data_root}')
+    imgs = sorted(glob(join(args.data_root, f'*{args.ext}')))
     imgs = list_to_tensor(parallel_execution(imgs, action=load_image), args.device)  # rgb images: B, C, H, W
     down = resize_image_tensor(imgs, args.ratio)
     B, C, H, W = imgs.shape
     B, C, nH, nW = down.shape
 
     # Perform feature detection (use kornia implementation)
+    log(f'Performing feature detection using: {args.device}')
     feature_detector = FeatureDetector().to(args.device, non_blocking=True)  # the actual feature detector
     with torch.no_grad():
-        lafs, descs, resp = feature_detector(down)  # B, N, 128 -> batch size, number of descs, desc dimensionality
+        lafs, desc, resp = feature_detector(down)  # B, N, 128 -> batch size, number of descs, desc dimensionality
 
-    # TODO: visualize feature detection results
+    # Visualize feature detection results as images
+    log(f'Visualizing detected feature')
+    visualize_detection(imgs,
+                        lafs,
+                        resp,
+                        [join(args.data_root, args.output_dir, f'detect_{i:02d}.jpg') for i in range(B)],
+                        args.ratio)
 
     # Perform feature matching
-    pivot, match = feature_matching(descs, args.match_ratio)  # M, 3 -> image id, source feat id, target feat id
+    log(f'Performing exhaustive feature matching on: {args.device}')
+    pivot, match = feature_matching(desc, args.match_ratio)  # M, 3 -> image id, source feat id, target feat id
 
     # Visualize feature matching results
+    log(f'Visualizing feature matching results')
     resp_pivot = resp[pivot:pivot + 1]  # 1, C, 2, 3
     lafs_pivot = lafs[pivot:pivot + 1]  # 1, C, 2, 3
     imgs_pivot = imgs[pivot:pivot + 1]  # 1, C, H, W
@@ -230,18 +309,58 @@ def main():
     packed_pivot = pack_laf_into_opencv_fmt(lafs_pivot, resp_pivot)  # 1, N, 4
     packed = pack_laf_into_opencv_fmt(lafs, resp)  # B-1, N, 4
 
-    # TODO: visualize feature matching results & log feature matching results
+    def get_actual_idx(idx, pivot=pivot): return idx if idx < pivot else idx + 1
+    visualize_matches(imgs_pivot,
+                      lafs_pivot,
+                      resp_pivot,
+                      imgs,
+                      lafs,
+                      resp,
+                      match,
+                      [join(args.data_root, args.output_dir, f'match_{pivot:02d}-{get_actual_idx(i):02d}.jpg') for i in range(B)],
+                      args.ratio)
 
     # Construct matched feature pairs from matching results
     pvt = packed_pivot[0, match[..., 1], :2]  # M, 2, discarding scale, orientation, and response
     src = packed[match[..., 0], match[..., 2], :2]  # M, 2
     pairs = torch.cat([match, pvt, src], dim=-1)  # M, 7 -> source image id, source feat id, target feat id, pivot xy, source xy
     pairs = pairs[torch.argsort(pairs[..., 0])]  # sort pairs by source image id
-    pvt, src = pairs[..., -2 - 2:-2], pairs[..., -2:]  # sorted pairs by source image id
+    pvt, src = pairs[..., -2 - 2:-2], pairs[..., -2:]  # sorted pairs by source image id, M, 2; M, 2
 
     # Perform RANSAC and homography one by one for target image ids
     uni, inv, cnt, ind = unique_with_indices(pairs[..., 0], sorted=True)
-    __import__('ipdb').set_trace()
+
+    # For now, only consider matched points of the first image?
+    for idx, curr, next in zip(uni, ind, torch.cat([ind[1:], ind.new_tensor([len(pairs)])])):
+        idx = int(idx)  # MARK: SYNC
+        curr = int(curr)  # MARK: SYNC
+        next = int(next)  # MARK: SYNC
+        # next_appearance - first_appearance records the size
+        size = next - curr
+        log(f'processsing image pair: {colored(f"{pivot:02d}-{get_actual_idx(idx):02d}", "magenta")}, matches: {colored(f"{size}", "magenta")}')
+        pvt_pair = pvt[curr:next]
+        src_pair = src[curr:next]
+        pvt_pair = pvt_pair / args.ratio  # use original image when stitching
+        src_pair = src_pair / args.ratio  # use original image when stitching
+        homography = discrete_linear_transform(pvt_pair, src_pair)
+        min_x, min_y, max_x, max_y, trans = homography_transform(imgs[idx], homography)
+        save_image(join(args.data_root, args.output_dir, f'trans_{pivot:02d}-{get_actual_idx(idx):02d}.jpg'), trans.permute(1, 2, 0).detach().cpu().numpy())
+        # break  # how do we deal with multiple blending?
+
+    # # Determine canvas size:
+    # pvt_min_x, pvt_min_y = min(0, src_min_x), min(0, src_min_y)
+    # pvt_max_x, pvt_max_y = max(W, src_max_x), max(H, src_max_y)
+    # pvt_W, pvt_H = pvt_max_x - pvt_min_x, pvt_max_y - pvt_min_y
+
+    # # Linear blending for now (excess memory usage?)
+    # pvt_canvas = pvt.new_zeros((C, int(pvt_H), int(pvt_W)))  # 3, pH, pW
+    # src_canvas = pvt.new_zeros((C, int(pvt_H), int(pvt_W)))  # 3, pH, pW
+    # pvt_canvas[-pvt_min_y:H, -pvt_min_x:W] = pvt
+    # src_canvas[-pvt_min_y - src_min_y:src_H, -pvt_min_x - src_min_x:src_W] = src
+    # blended = (pvt_canvas + src_canvas) / 2
+    # return blended
+
+    save_image(join(args.data_root, args.output_dir, 'merged.jpg'), trans.permute(1, 2, 0).detach().cpu().numpy())
 
 
 if __name__ == "__main__":
